3e27a78fd41db248f971bd279fcfe4fabeaade37
namburiamit <anamburi@ucsd.edu>
2023-05-28 23:57:08 -0700

    final changes

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -4,7 +4,7 @@ test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
 });
 
-
+const sum = require('../code-to-unit-test/sum.js');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
